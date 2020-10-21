54676007429d017507365f100f9bd47a379e2668
Keir Lawson <keirlawson@gmail.com>
2020-10-21 22:56:49 +0100

    format

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,61 @@
-use url::Url;
-use std::fmt;
+use anyhow::{anyhow, Context, Result};
+use dialoguer::{theme::ColorfulTheme, Select};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use serde::{Serialize, Deserialize};
-use anyhow::{Context, Result, anyhow};
-use dialoguer::{
-    Select,
-    theme::ColorfulTheme
-};
+use std::fmt;
+use url::Url;
 
 use reqwest::blocking::Client;
 
 #[derive(Serialize)]
 struct IssueSearchRequestBody {
     jql: String,
-    fields: Vec<String>
+    fields: Vec<String>,
 }
 
 #[derive(Deserialize)]
 struct IssueSearchResponseBody {
-    issues: Option<Vec<Issue>>
+    issues: Option<Vec<Issue>>,
 }
 
 #[derive(Deserialize)]
 pub struct Issue {
     pub key: String,
-    pub fields: HashMap<String, String>
+    pub fields: HashMap<String, String>,
 }
 
 impl fmt::Display for Issue {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { write!(f, "{} - {}", &self.key, &self.fields.get("summary").unwrap()) }
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} - {}",
+            &self.key,
+            &self.fields.get("summary").unwrap()
+        )
+    }
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
     jira_host: Url,
     jira_user: String,
-    jira_password: Option<String>
+    jira_password: Option<String>,
 }
 
 pub fn search_issues(config: Config, query: &str) -> Result<Vec<Issue>> {
     let body = IssueSearchRequestBody {
         jql: String::from(query),
-        fields: vec!(String::from("summary"))
+        fields: vec![String::from("summary")],
     };
 
     let resp = Client::new()
         .post(config.jira_host.join("/rest/api/2/search").unwrap())
         .json(&body)
         .basic_auth(config.jira_user, config.jira_password)
-        .send().context("Unable to search JIRA for issues")?
-        .json::<IssueSearchResponseBody>().context("Unable to decode JIRA response")?;
+        .send()
+        .context("Unable to search JIRA for issues")?
+        .json::<IssueSearchResponseBody>()
+        .context("Unable to decode JIRA response")?;
 
     resp.issues.ok_or(anyhow!("No issues found for query"))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
-use anyhow::{Context, Result, anyhow};
+use anyhow::{anyhow, Context, Result};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
 struct Opt {
-    #[structopt(default_value = "assignee IN (currentUser()) AND sprint IN openSprints() ORDER BY created DESC")]
-    query: String
+    #[structopt(
+        default_value = "assignee IN (currentUser()) AND sprint IN openSprints() ORDER BY created DESC"
+    )]
+    query: String,
 }
 
 fn main() -> Result<()> {
     let opt = Opt::from_args();
 
-    let config = envy::from_env::<ji::Config>().map_err(|e| {
-        match e {
-            envy::Error::MissingValue(value) => anyhow!("Required environment variable {} not set", value.to_uppercase()),
-            envy::Error::Custom(message) => anyhow!("{}", message)
-        }
-    }).context("Unable to load configuration from environment variables")?;
+    let config = envy::from_env::<ji::Config>()
+        .map_err(|e| match e {
+            envy::Error::MissingValue(value) => anyhow!(
+                "Required environment variable {} not set",
+                value.to_uppercase()
+            ),
+            envy::Error::Custom(message) => anyhow!("{}", message),
+        })
+        .context("Unable to load configuration from environment variables")?;
 
     let issues = ji::search_issues(config, &opt.query)?;
 
